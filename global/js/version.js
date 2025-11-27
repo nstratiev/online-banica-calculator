f4f462c6a32b5f3b5d3349d95dc3ffa2e8d14dda
Nikolay Stratiev <n.stratiev@gmail.com>
2025-11-27 11:46:48 +0200

    Fix Print Current Version

--- global/js/version.js
+++ global/js/version.js
@@ -2,9 +2,14 @@ const appVersion = 'Версия: 1.1.3';
 const appEnvironmentType = '';
 
 export function printCurrentAppVersion(versionElem) {
-  versionElem.textContent = appVersion;
+  const fragment = new DocumentFragment();
+
+  fragment.textContent = appVersion;
 
   const spanElem = document.createElement('span');
   spanElem.textContent = appEnvironmentType;
-  versionElem.appendChild(spanElem);
+  fragment.appendChild(spanElem);
+
+  // versionElem.innerHTML = '';
+  versionElem.appendChild(fragment);
 }
